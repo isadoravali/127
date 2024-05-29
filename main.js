341ca9b06dd12cff6a2dd230d03f44fe77bf1772
isadoravali <126509584+isadoravali@users.noreply.github.com>
2024-05-29 14:16:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log(quickDatataset [randonNumber]);
+sketch = quickDrawDataset [randowNumber];
+timerCounter = 0;
+timerCheck = [];
+drawSketch = [];
+ansewerHolder = [];
+score = 0;
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
